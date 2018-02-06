215cf27e850776cc1bd5cbf80601b59dfcc133db
rakeshvora2007 <rakeshvora2007@gmail.com>
2018-02-05 23:56:41 -0800

    removed logs

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,7 +1,6 @@
 import { FETCH_WEATHER } from '../actions/index';
 
 export default function(state=[], action) {
-    console.log(action);
     switch(action.type) {
         case FETCH_WEATHER:
         return[action.payload.data, ...state]
