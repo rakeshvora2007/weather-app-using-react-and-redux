7caba98fb3329108394d04c79066d48ad7cb1983
Rakesh Jain <rakeshvora2007@gmail.com>
2019-07-18 13:25:34 -0400

    Updates

--- index.js
+++ index.js
@@ -46231,7 +46231,7 @@
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 	var API_KEY = "7d9be76ca8eff25bdc44ae9b82f48e7b";
-	var ROOT_URL = 'http://api.openweathermap.org/data/2.5/forecast?&appid=' + API_KEY;
+	var ROOT_URL = 'https://api.openweathermap.org/data/2.5/forecast?&appid=' + API_KEY;
 
 	var FETCH_WEATHER = exports.FETCH_WEATHER = 'FETCH_WEATHER';
 
