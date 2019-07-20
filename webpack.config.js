5f22f8452ce74873d855864b2c2a083a1a5a82c1
Rakesh Jain <rakeshvora2007@gmail.com>
2019-07-19 20:50:26 -0400

    styles resolved for production

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,39 @@
-// const path = require('path');
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-// const htmlWebpackPlugin = 
 
 module.exports = {
   entry: __dirname + "/src/index.js",
   output: {
     path: __dirname + "/build",
-    filename: 'index.js'
+    filename: "index.js"
   },
   module: {
-    loaders: [{
-      test: /\.js?$/,
-      exclude: /node_modules/,
-      loader: 'babel',
-      query: {
-        presets: ['react', 'es2015', 'stage-1']
+    loaders: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        loader: "babel",
+        query: {
+          presets: ["react", "es2015", "stage-1"]
+        }
+      },
+      {
+        test: /\.css$/,
+        loaders: ["style-loader", "css-loader?modules=true"]
       }
-    }]
+    ]
   },
   resolve: {
-    extensions: ['', '.js', '.jsx']
+    extensions: ["", ".js", ".jsx"]
   },
   devServer: {
     historyApiFallback: true,
-    contentBase: './'
+    contentBase: "./"
   },
-  plugins: [new HtmlWebpackPlugin({
-    template: __dirname + "/index.html",
-    filename: __dirname + '/build/index.html',
-    inject: 'body'
-})]
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: __dirname + "/index.html",
+      filename: __dirname + "/build/index.html",
+      inject: "body"
+    })
+  ]
 };
