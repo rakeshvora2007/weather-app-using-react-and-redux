7aeb589ae05cc65b28f7915f0eeb53d465759ea3
Rakesh Jain <rakeshvora2007@gmail.com>
2019-07-22 21:41:03 -0400

    duplicate issue fixed with latest being on top

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,53 +1,59 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import Chart from '../components/chart';
-
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import Chart from "../components/chart";
 
 class WeatherList extends Component {
+  renderWeather(cityData) {
+    const name = cityData.city.name;
+    const temps = cityData.list.map(weather => weather.main.temp);
+    const pressure = cityData.list.map(weather => weather.main.pressure);
+    const humidity = cityData.list.map(weather => weather.main.humidity);
+    return (
+      <tr key={name}>
+        <td>{cityData.city.name}</td>
+        <td>
+          <Chart data={temps} color="red" units="K" />
+        </td>
+        <td>
+          <Chart data={pressure} color="orange" units="hPa" />
+        </td>
+        <td>
+          <Chart data={humidity} color="grey" units="%" />
+        </td>
+      </tr>
+    );
+  }
 
-    renderWeather(cityData) {
-        const name = cityData.city.name;
-        const temps = cityData.list.map(weather => weather.main.temp);
-        const pressure = cityData.list.map(weather => weather.main.pressure);
-        const humidity = cityData.list.map(weather => weather.main.humidity);
-        return (
-            <tr key={name}>
-                <td>{cityData.city.name}</td>
-                <td>
-                    <Chart data={temps} color="red" units="K"/>
-                </td>
-                <td>
-                    <Chart data={pressure} color="orange" units="hPa"/>
-                </td>
-                <td>
-                    <Chart data={humidity} color="grey" units="%"/>
-                </td>
-            </tr>
-        );
-    }
+  removeDuplicates(arr) {
+    const uniqueAddresses = Array.from(new Set(arr.map(a => a.city.id))).map(
+      id => {
+        return arr.find(a => a.city.id === id);
+      }
+    );
+    return uniqueAddresses;
+  }
 
-    render() {
-        return (
-            <table className="table table-hover">
-                <thead>
-                    <tr>
-                    <th> CITY </th>
-                    <th> TEMPERATURE (K) </th>
-                    <th> PRESSURE (hPa) </th>
-                    <th> HUMIDITY (%) </th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {this.props.weather.map(this.renderWeather)}
-                </tbody>
-            </table>
-        );
-
-    }
+  render() {
+    return (
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th> CITY </th>
+            <th> TEMPERATURE (K) </th>
+            <th> PRESSURE (hPa) </th>
+            <th> HUMIDITY (%) </th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.removeDuplicates(this.props.weather).map(this.renderWeather)}
+        </tbody>
+      </table>
+    );
+  }
 }
 
 function mapStateToProps({ weather }) {
-    return { weather: weather };
+  return { weather: weather };
 }
 
 export default connect(mapStateToProps)(WeatherList);
