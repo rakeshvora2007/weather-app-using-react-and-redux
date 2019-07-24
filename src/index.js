49fba978e310e092275a3a9682b2b8762d5eea17
Rakesh Jain <rakeshvora2007@gmail.com>
2019-07-23 20:50:09 -0400

    redux-logger for logging api request

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { createLogger } from 'redux-logger';
 import ReduxPromise from 'redux-promise';
 import '../style/style.css';
 
 import App from './components/app';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
+const createStoreWithMiddleware = applyMiddleware(createLogger(), ReduxPromise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
