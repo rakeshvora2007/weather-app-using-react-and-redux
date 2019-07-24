e2777becaed004705d5642d805c812eef0232254
Rakesh Jain <rakeshvora2007@gmail.com>
2019-07-23 22:28:58 -0400

    if city not found detection

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -2,8 +2,10 @@ import { FETCH_WEATHER } from "../actions/index";
 
 export default function(state = [], action) {
   switch (action.type) {
-    case FETCH_WEATHER:
-      if (action.payload.data !== undefined) {
+    case 'FETCH_WEATHER':
+    if(action.error) {
+      console.log("error");
+    } else if (action.payload.data !== undefined) {
         return [action.payload.data, ...state];
       }
   }
